b1e9023b6d25384bee6cde1cf477eda07192e839
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2020-10-26 17:15:45 +0100

    Add files via upload

---
+++ Example/App_1.py
@@ -0,0 +1,30 @@
+'''
+Created on 20 ott 2020
+
+@author: andrea
+'''
+import angel
+
+version = 0.3.1
+ 
+
+Folder_Sync_App = '/home/andrea/SynC'
+App_Name = 'AngelTest'
+add = 1
+s = angel.Angel(Folder_Sync_App, App_Name, add)
+print('Angel Version: ',s.AngelVersion+' @ APP_1')
+
+while True:
+    nn = input('For Read=r, For Write=w:, For Exit=e ')
+    if nn == 'r':
+        x=s.Read(delete=False)
+        #print('Letto dallo slave..>',x)
+        if x is not False:
+            for z in x:
+                print (z)
+    elif nn == 'w':
+        cc = input('Inserisci stringa--> ')
+        s.Write(cc, 2, 1)
+    elif nn == 'e':
+        break
+    #time.sleep(2)
